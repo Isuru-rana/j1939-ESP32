49d694f67bf894a66224e2df341336b43d8ea17f
Malachi Burke <malachi.burke@gmail.com>
2024-07-09 10:27:53 -0700

    Adding non-config resiliency

--- src/can/can/transport.cpp
+++ src/can/can/transport.cpp
@@ -10,10 +10,20 @@ namespace internal {
 // wanna wrap all that up here)
 void twai_transport::init()
 {
+#ifdef CONFIG_TWAI_DISABLED
+    // DEBT: Decide what to do here - should we have a linker err? runtime
+    // error?
+#else
     static twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(
         (gpio_num_t)CONFIG_GPIO_TWAI_TX,
         (gpio_num_t)CONFIG_GPIO_TWAI_RX,
+#if CONFIG_TWAI_MODE_NORMAL
         TWAI_MODE_NORMAL);  // DEBT: Pull this in from config
+#elif CONFIG_TWAI_LISTEN_ONLY
+        TWAI_LISTEN_ONLY);
+#else
+#error  Invalid TWAI listen mode
+#endif
     static const twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 
     static const twai_timing_config_t t_config =
@@ -33,6 +43,7 @@ void twai_transport::init()
 
     ESP_ERROR_CHECK(twai_driver_install(&g_config, &t_config, &f_config));
     ESP_ERROR_CHECK(twai_start());
+#endif
 }
 
 }
